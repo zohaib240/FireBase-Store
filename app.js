ca291fc6a1b08900baf87be68985784fa68f14f9
zohaib240 <zohaibjs123@gmail.com>
2024-08-04 22:43:08 -0700

    delete edit changed

--- app.js
+++ app.js
@@ -1,115 +1,85 @@
 
+
+
 import {
   collection,
   addDoc,
   getDocs,
-deleteDoc} from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
+  deleteDoc,
+  updateDoc,
+  doc
+} from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
 import { Db } from "./config.js";
-
 
 const form = document.querySelector("#form");
 const todo = document.querySelector("#todo");
 const ul = document.querySelector("#ul");
-let arr=[];
+let arr = [];
 
-function renderTodo () {
-  ul.innerHTML=''
-  if (arr.length===0) {
-      ul.innerHTML='no datafound'
-      return
-  }else{
-  arr.map((item)=>{
- ul.innerHTML+=`<li>${item.todo} 
-  <button type='submit' class="deletebtn">Delete todo</button> 
-  <button type='submit' class="editbtn">Edit todo</button></li>`
-  })
-   
+// Render todos
+function renderTodo() {
+  ul.innerHTML = '';
+  if (arr.length === 0) {
+    ul.innerHTML = 'No data found';
+    return;
   }
+  arr.forEach((item, index) => {
+    ul.innerHTML += `
+      <li>
+        ${item.todo} 
+        <button type='button' class="deletebtn" data-index="${index}">Delete todo</button> 
+        <button type='button' class="editbtn" data-index="${index}">Edit todo</button>
+      </li>`;
+  });
 }
 
-
+// Fetch and display todos
 async function getData() {
-  arr=[]
+  arr = [];
   const querySnapshot = await getDocs(collection(Db, "todos"));
   querySnapshot.forEach((doc) => {
-    console.log(doc.data());
-    arr.push(doc.data());
+    arr.push({ id: doc.id, todo: doc.data().todo });
   });
-  console.log(arr);
   renderTodo();
-
 }
 
 getData();
 
+// Add a new todo
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  const newTodo = todo.value.trim();
+  if (newTodo === '') return;
 
+  try {
+    const docRef = await addDoc(collection(Db, "todos"), { todo: newTodo });
+    arr.push({ id: docRef.id, todo: newTodo });
+    renderTodo();
+    todo.value = "";
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+});
 
-
-form.addEventListener('submit',async (event)=>{
-    event.preventDefault()
-  arr.push({
-    todo:todo.value,
-  })
-  console.log(arr)
-renderTodo()
-try {
-  const docRef = await addDoc(collection(Db, "todos"), {
-    todo: todo.value,
-  });
-  todo.value = "";
-  console.log("Document written with ID: ", docRef.id);
-} catch (e) {
-  console.error("Error adding document: ", e);
-}
-
-todo.value=''
-})
-
-
-// delete data from arry and firestore
-
-
-let deleteBtn = document.querySelector('.deletebtn')
-
-// deletebtn.forEach((btn,index)=>{
-
-//   btn.addEventListener('click',async()=>{
-//     console.log(arr[index]);
-//     await deleteDoc(doc(Db,'todos',arr[index].id))
-//     console.log('delete data')
-//     arr.splice(index,1)
-//     renderTodo()
-//   })
-// })
-
-deleteBtn.forEach((btn, index) => {
-  btn.addEventListener("click", async () => {
-    console.log(arr[index]);
-    await deleteDoc(doc(Db, "todos", arr[index].id));
-    console.log("Data deleted");
+// Event delegation for delete and edit buttons
+ul.addEventListener('click', async (event) => {
+  const target = event.target;
+  const index = target.getAttribute('data-index');
+  if (target.classList.contains('deletebtn')) {
+    const todoId = arr[index].id;
+    await deleteDoc(doc(Db, "todos", todoId));
     arr.splice(index, 1);
     renderTodo();
-  });
-});
+  } else if (target.classList.contains('editbtn')) {
+    const updatedNewValue = prompt("Enter new value", arr[index].todo);
+    if (updatedNewValue === null || updatedNewValue.trim() === '') return;
 
-//  edit from arry and firestore
-
-
-let editBtn = document.querySelector('.editbtn')
-editBtn.forEach((btn, index) => {
-  btn.addEventListener("click", async () => {
-    const updatedNewValue = prompt("enter new value");
-    const todoUpdate = doc(Db, "todos", arr[index].id);
-    await updateDoc(todoUpdate, {
-      todo: updatedNewValue,
-    });
-    console.log("update");
+    const todoId = arr[index].id;
+    await updateDoc(doc(Db, "todos", todoId), { todo: updatedNewValue });
     arr[index].todo = updatedNewValue;
     renderTodo();
-  });
+  }
 });
-
-
 
 
 
